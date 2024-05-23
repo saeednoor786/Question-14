5816f77336cde5af943253c5fc4c88ebf31d995e
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:21:43 +0500

    fourty assignment

---
+++ app.ts
@@ -0,0 +1,21 @@
+//Q:14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+// Make a list that includes at least three people you’d like to invite to dinner. 
+// Then use your list to print a message to each person, inviting them to dinner.
+
+let guest_list: string [] = ["salman", "ahmed","saeed","noor"];
+
+for (let i = 0; i < guest_list.length; i++){
+    console.log(`Dear ${guest_list[i]} : \n you are invite to dinner! \n`);
+
+};
+
+
+
+
+
+
+
+
+
+
+
